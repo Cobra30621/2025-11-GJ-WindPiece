792c4e46a9972ea1bc99841f45ec2373b7ea0c91
Ibuprofenkimo <chinghung0913@gmail.com>
2025-11-16 16:59:12 +0800

    End Animation

--- Assets/Scripts/UI/Main/ResultManager.cs
+++ Assets/Scripts/UI/Main/ResultManager.cs
@@ -1,17 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ResultManager : MonoBehaviour
+[System.Serializable]
+public struct SpriteGroup
 {
-    [SerializeField] private string titleSceneName = "Title"; // 回到的主選單場景名
+    public Sprite[] frames; // 你可在 Inspector 為每組放 4 張
+}public class ResultManager : MonoBehaviour
+{
+    [SerializeField] private string titleSceneName = "Title";
+    [SerializeField] private Image display;
+    [SerializeField] private List<SpriteGroup> groups;
 
-    void Update()
+    [SerializeField] private float frameTime = 0.1f;
+    [SerializeField] private float groupDuration = 1f;
+
+    void Start()
     {
-        // 按空白鍵回主選單
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        StartCoroutine(PlayAllGroups());
+    }
+
+    IEnumerator PlayAllGroups()
+    {
+        foreach (var group in groups)
         {
-            SceneManager.LoadScene(titleSceneName);
+            yield return StartCoroutine(PlayOneGroup(group));
+        }
+
+        SceneManager.LoadScene(titleSceneName);
+    }
+
+    IEnumerator PlayOneGroup(SpriteGroup group)
+    {
+        Sprite[] frames = group.frames;   // 取出這組的 4 張圖
+
+        if (frames == null || frames.Length == 0)
+            yield break;
+
+        float timer = 0f;
+        int index = 0;
+
+        while (timer < groupDuration)
+        {
+            display.sprite = frames[index];
+            index = (index + 1) % frames.Length;
+
+            yield return new WaitForSeconds(frameTime);
+            timer += frameTime;
         }
     }
 }
